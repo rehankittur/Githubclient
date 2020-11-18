fdf3e0335a2c2147e6cdd975f88a19d35d05c180
rehan <mohammed.kittur@nttdata.com>
2020-11-18 18:26:22 +0530

    efe
    fe

--- EmployeeAppPWA/ClientPWA/src/api/baseURL.js
+++
@@ -1,4 +0,0 @@
-// get base URL for API calls
-export const apiUrl = window.location.origin.includes('localhost')
-  ? 'https://uniteemployeeapp-uat.azurewebsites.net'
-  : window.location.origin;
--- EmployeeAppPWA/ClientPWA/src/store/types/index.js
+++
@@ -1,2 +0,0 @@
-// export all types from index, so as to minimize the number of files imported when required
-export *  from './checkinTypes';
